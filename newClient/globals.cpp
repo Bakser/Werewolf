bcc0875d98a7b6cecf14df1729b16072d3301b3c
SherlynW <wangsysusan@gmail.com>
2017-05-29 17:42:47 +0800

    Update globals.cpp

--- newClient/globals.cpp
+++ newClient/globals.cpp
@@ -4,8 +4,8 @@ QString Globals::meid="4";//自己的username
 int Globals::roomnumber=222;
 
 //根据包改，此处为测试
-bool Globals::player_alive[20]={1,1,1,1,1,1,1,1,1,1};
-QString Globals::player_id[20]={"231","b","qw","sdf","d","d","23fe","d","sd","ers"};
+bool Globals::player_alive[50]={1,1,1,1,1,1,1,1,1,1};
+QString Globals::player_id[50]={"231","b","qw","sdf","d","d","23fe","d","sd","ers"};
 int Globals::nplayer=10;
 
 int Globals::merole=2;//自己的身份
